373935acb1ea55e1cd9e9c9eca15716618f44a69
Edelways <113142949+jeffpharaon@users.noreply.github.com>
2024-10-07 20:38:10 +0300

    Create Output-Input.cs

---
+++ HomeWork/Output-Input.cs
@@ -0,0 +1,18 @@
+// 1. ЗАДАНИЕ: Исправьте ошибки в коде и допишите код
+// ВЫВОД: Name: Alex Age: 20
+ 
+var name = Alex;
+var age = "20";
+
+Console.WriteLine("Name: {name} " + "Age: {age}");
+
+// 2. ЗАДАНИЕ: Реализуйте ввод имени и возраста через пользовательский ввод
+// ВЫВОД: Name: (какое-то имя) Age: (какой-то возраст)
+
+Console.WriteLine();
+
+// 3. ЗАДАНИЕ: Перенесите вторую часть текста на следующую строку
+// ВЫВОД: Text Part One
+// Text Part Two
+
+Console.WriteLine("Text Part One" + "Text Part Two");
